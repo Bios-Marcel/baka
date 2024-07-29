30d19809f4d32b8689a43ceac76c9e0679844d62
Marcel Schramm <marceloschr@googlemail.com>
2024-07-29 22:39:42 +0200

    Add more filtering options
    * Tags
      * has (equal on each tag, so more of a "has tag $1")
    * effective_date / booking_date
      * !=
    * account.iban
      * =
      * !=
      * has
    * amount
      * =
      * !=

--- src/main/java/link/biosmarcel/baka/view/PaymentFilter.java
+++ src/main/java/link/biosmarcel/baka/view/PaymentFilter.java
@@ -19,12 +19,18 @@ public class PaymentFilter extends Filter<Payment> {
 
         register("account", Operator.EQ, (payment, value) -> payment.account.name != null && payment.account.name.equalsIgnoreCase(value), String::toLowerCase);
         register("account", Operator.NOT_EQ, (payment, value) -> payment.account.name == null || !payment.account.name.equalsIgnoreCase(value), String::toLowerCase);
-        register("account", Operator.HAS, (payment, value) -> payment.account.name != null && !payment.account.name.toLowerCase().contains(value), String::toLowerCase);
+        register("account", Operator.HAS, (payment, value) -> payment.account.name != null && payment.account.name.toLowerCase().contains(value), String::toLowerCase);
+
+        register("account.iban", Operator.EQ, (payment, value) -> payment.account.iban != null && payment.account.iban.equalsIgnoreCase(value), String::toLowerCase);
+        register("account.iban", Operator.NOT_EQ, (payment, value) -> payment.account.iban == null || !payment.account.iban.equalsIgnoreCase(value), String::toLowerCase);
+        register("account.iban", Operator.HAS, (payment, value) -> payment.account.iban != null && payment.account.iban.toLowerCase().contains(value), String::toLowerCase);
 
         register("amount", Operator.LT, (payment, value) -> payment.amount.compareTo(value) < 0, BigDecimal::new);
         register("amount", Operator.GT, (payment, value) -> payment.amount.compareTo(value) > 0, BigDecimal::new);
         register("amount", Operator.LT_EQ, (payment, value) -> payment.amount.compareTo(value) <= 0, BigDecimal::new);
         register("amount", Operator.GT_EQ, (payment, value) -> payment.amount.compareTo(value) >= 0, BigDecimal::new);
+        register("amount", Operator.EQ, (payment, value) -> payment.amount.compareTo(value) == 0, BigDecimal::new);
+        register("amount", Operator.NOT_EQ, (payment, value) -> payment.amount.compareTo(value) != 0, BigDecimal::new);
 
         register("name", Operator.EQ, (payment, value) -> payment.name.equalsIgnoreCase(value), String::toLowerCase);
         register("name", Operator.NOT_EQ, (payment, value) -> !payment.name.equalsIgnoreCase(value), String::toLowerCase);
@@ -43,11 +49,15 @@ public class PaymentFilter extends Filter<Payment> {
         register("effective_date", Operator.LT_EQ, (payment, value) -> payment.effectiveDate.getLong(ChronoField.EPOCH_DAY) <= value, value -> LocalDate.parse(value, DATE_FORMAT).toEpochDay());
         register("effective_date", Operator.GT_EQ, (payment, value) -> payment.effectiveDate.getLong(ChronoField.EPOCH_DAY) >= value, value -> LocalDate.parse(value, DATE_FORMAT).toEpochDay());
         register("effective_date", Operator.EQ, (payment, value) -> payment.effectiveDate.getLong(ChronoField.EPOCH_DAY) == value, value -> LocalDate.parse(value, DATE_FORMAT).toEpochDay());
+        register("effective_date", Operator.NOT_EQ, (payment, value) -> payment.effectiveDate.getLong(ChronoField.EPOCH_DAY) != value, value -> LocalDate.parse(value, DATE_FORMAT).toEpochDay());
 
         register("booking_date", Operator.LT, (payment, value) -> payment.bookingDate.isBefore(value), value -> LocalDate.parse(value, DATE_FORMAT).atStartOfDay());
         register("booking_date", Operator.GT, (payment, value) -> payment.bookingDate.isAfter(value), value -> LocalDate.parse(value, DATE_FORMAT).atTime(LocalTime.MAX));
         register("booking_date", Operator.LT_EQ, (payment, value) -> payment.bookingDate.getLong(ChronoField.EPOCH_DAY) <= value, value -> LocalDate.parse(value, DATE_FORMAT).toEpochDay());
         register("booking_date", Operator.GT_EQ, (payment, value) -> payment.bookingDate.getLong(ChronoField.EPOCH_DAY) >= value, value -> LocalDate.parse(value, DATE_FORMAT).toEpochDay());
         register("booking_date", Operator.EQ, (payment, value) -> payment.bookingDate.getLong(ChronoField.EPOCH_DAY) == value, value -> LocalDate.parse(value, DATE_FORMAT).toEpochDay());
+        register("booking_date", Operator.NOT_EQ, (payment, value) -> payment.bookingDate.getLong(ChronoField.EPOCH_DAY) != value, value -> LocalDate.parse(value, DATE_FORMAT).toEpochDay());
+
+        register("tags", Operator.HAS, (payment, value) -> payment.classifications.stream().anyMatch(classification -> classification.tag.equalsIgnoreCase(value)), String::toLowerCase);
     }
 }
